306d43d49619c6911d2ca429c43b6d1e355fee1f
王春成 <chuncheng.wang@hand-china.com>
2019-03-25 10:56:22 +0800

    使用ant design,配置按需加载,less加载器

---
+++ config-overrides.js
@@ -0,0 +1,18 @@
+const {override, useBabelRc, fixBabelImports, addLessLoader} = require('customize-cra');
+
+module.exports = override(
+    // 使用Babel-plugin-import按需加载
+    fixBabelImports('import', {
+        libraryName: 'antd',
+        libraryDirectory: 'es',
+        // style: 'css', //仅使用按需加载
+        style: true // 使用配置主题
+    }),
+    // 配置主题 yarn add less less-loader
+    addLessLoader({
+        javascriptEnabled: true,
+        modifyVars: {'@primary-color': '#1DA57A'},
+    }),
+    // 使用.babelrc
+    // useBabelRc()
+);
