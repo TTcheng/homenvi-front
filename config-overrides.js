b518a2ba4c4536c59796ac86f3b32d6361743a4a
WangChuncheng <ttchengwang@foxmail.com>
2021-03-02 23:45:10 +0800

    [ADD] 接口代理

--- config-overrides.js
+++ config-overrides.js
@@ -1,19 +1,38 @@
-const {override, addDecoratorsLegacy, useBabelRc, fixBabelImports, addLessLoader} = require('customize-cra');
+const {
+    override,
+    addDecoratorsLegacy,
+    useBabelRc,
+    fixBabelImports,
+    addLessLoader,
+    overrideDevServer
+} = require('customize-cra');
 
-module.exports = override(
-    // 使用Babel-plugin-import按需加载
-    fixBabelImports('import', {
-        libraryName: 'antd',
-        libraryDirectory: 'es',
-        // style: 'css', //仅使用按需加载
-        style: true // 使用配置主题
-    }),
-    // 配置主题 yarn add less less-loader
-    addLessLoader({
-        javascriptEnabled: true,
-        modifyVars: {'@primary-color': '#1DA57A'},
-    }),
-    addDecoratorsLegacy(),
-    // 使用.babelrc
-    // useBabelRc()
-);
+module.exports = {
+    webpack: override(
+        // 使用Babel-plugin-import按需加载
+        fixBabelImports('import', {
+            libraryName: 'antd',
+            libraryDirectory: 'es',
+            // style: 'css', //仅使用按需加载
+            style: true // 使用配置主题
+        }),
+        // 配置主题 yarn add less less-loader
+        addLessLoader({
+            javascriptEnabled: true,
+            modifyVars: {'@primary-color': '#1DA57A'},
+        }),
+        addDecoratorsLegacy(),
+        // 使用.babelrc
+        // useBabelRc()
+    ),
+    devServer: overrideDevServer( (configFunction) => {
+        configFunction.proxy = {
+            '/api/': {
+                target: 'http://localhost:8080',
+                changeOrigin: true,
+                pathRewrite: {'^/api': '/'},
+            },
+        };
+        return configFunction;
+    })
+};
